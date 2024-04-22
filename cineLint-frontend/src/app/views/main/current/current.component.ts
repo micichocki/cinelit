8aff9691afc6aeaf183f54d8ac42c50d43246a67
MPT-B <maciej.biegan@student.pk.edu.pl>
2024-04-22 08:23:23 +0200

    feat(typography): Created simple typography classes

--- cineLint-frontend/src/app/views/main/current/current.component.ts
+++ cineLint-frontend/src/app/views/main/current/current.component.ts
@@ -1,16 +1,16 @@
-import { Component } from '@angular/core';
-import { MainStoreService } from '../store/main-store.service';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatCardModule } from '@angular/material/card';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatButtonModule } from '@angular/material/button';
+import { Component } from "@angular/core";
+import { MainStoreService } from "../store/main-store.service";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatCardModule } from "@angular/material/card";
+import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { MatProgressBarModule } from "@angular/material/progress-bar";
+import { MatIconModule } from "@angular/material/icon";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatButtonModule } from "@angular/material/button";
 
 @Component({
-  selector: 'app-current',
+  selector: "app-current",
   standalone: true,
   imports: [
     MatGridListModule,
@@ -21,8 +21,8 @@ import { MatButtonModule } from '@angular/material/button';
     MatDividerModule,
     MatIconModule,
   ],
-  templateUrl: './current.component.html',
-  styleUrl: './current.component.scss',
+  templateUrl: "./current.component.html",
+  styleUrl: "./current.component.scss",
 })
 export class CurrentComponent {
   isSmallScreen = false;
@@ -39,6 +39,6 @@ export class CurrentComponent {
   }
 
   ngOnInit() {
-    this.store.setCurrentSubpage('Current');
+    this.store.setCurrentSubpage("Current");
   }
 }
